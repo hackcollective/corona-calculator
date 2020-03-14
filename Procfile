e2f1e42ff6bf72b2bfe91b0604ede2034c30b173
Archy de Berler <archy.deberker@gmail.com>
2020-03-14 12:59:07 -0400

    Add Heroku procfile and basic streamlit app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python streamlit run app.py
---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def run_app():
+    st.title('Corona Calculator')
+    st.write('Graphs and analysis will go here')
+    st.sidebar.markdown('Settings will go here')
+
+
+if __name__ == '__main__':
+    run_app()
