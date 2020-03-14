684986a333db9dd9c4afa24bb7dd520c423bbcdb
Archy de Berler <archy.deberker@gmail.com>
2020-03-14 14:03:48 -0400

    Add more variables

--- app.py
+++ app.py
@@ -1,27 +1,43 @@
+from dataclasses import dataclass
+
 import streamlit as st
 import graphing
 
 # estimates from https://github.com/midas-network/COVID-19/tree/master/parameter_estimates/2019_novel_coronavirus
 
-def sidebar():
+
+@dataclass
+class sidebar():
     number_cases_confirmed = st.sidebar.number_input('Confirmed cases in your area', min_value=1, step=10)
 
-    estimated_doubling_time = st.sidebar.slider(label='Doubling time', min_value=1, max_value=10, value=5)
+    # Don't know if we want to present doubling time here or R or something else
+    estimated_doubling_time = st.sidebar.slider(label='Estimated time (days) for number of '
+                                                      'infected people to double', min_value=1, max_value=10, value=5)
 
     presentation_delay =  st.sidebar.slider(label='Time between infection and '
-                                                  'symptoms', min_value=1, max_value=10, value=5)
+                                                  'symptoms being displayed', min_value=1, max_value=10, value=5)
 
-    testing_delay = st.sidebar.slider(label='Time between symptoms '
-                                                 'diagnosis', min_value=1, max_value=10, value=3)
+    testing_delay = st.sidebar.slider(label='Time between being tested and receiving a diagnosis', min_value=1, max_value=10, value=3)
+
+    hospitalization_rate = st.sidebar.slider(label='% of cases requiring hospitalization',
+                                             min_value=0., max_value=100., step=.5, value=5.)
+
+    ventilator_rate = st.sidebar.slider(label='% of cases requiring a ventilator',
+                                             min_value=0., max_value=100., step=.5, value=1.)
+
+    num_beds = st.sidebar.slider(label='Number of hospital beds available'  
+                                'in your country', min_value=100, max_value=1000000, value=100000)
+
+    stay_in_hospital = st.sidebar.slider(label='How many days people stay in hospital', min_value=3, max_value=20, value=12)
 
 
 
 def run_app():
     st.title('Corona Calculator')
-    st.write('Graphs and analysis will go here')
     sidebar()
     figure = graphing.example_graph()
     st.write(figure)
+
 
 if __name__ == '__main__':
     run_app()
--- graphing.py
+++ graphing.py
@@ -5,3 +5,10 @@ def example_graph():
     df = px.data.iris()
     fig = px.scatter(df, x="sepal_width", y="sepal_length")
     return fig
+
+
+def infection_graph(df):
+    fig = px.scatter(df, x='Days', y='Confirmed Cases')
+    fig.add_scatter(df, x='Days', y='Actual Cases')
+    fig.add_scatter(df, x='Days', y='Patients in Hospital')
+    fig.add_scatter(df, x='Days', y='Deaths')
