94649f83b377617dd8e0796ac966fc442cdaa941
Pat Steeves <patrick.steeves@elementai.com>
2020-03-14 17:36:30 -0400

    use sir model for predictions

--- app.py
+++ app.py
@@ -1,55 +1,67 @@
 from dataclasses import dataclass
 
 import streamlit as st
+
 import graphing
 import models
+from data import constants
 
 # estimates from https://github.com/midas-network/COVID-19/tree/master/parameter_estimates/2019_novel_coronavirus
 
 
 @dataclass
-class sidebar():
-    number_cases_confirmed = st.sidebar.number_input('Confirmed cases in your area', min_value=1, step=10)
+class sidebar:
+    area_population = st.sidebar.number_input(
+        "Population in your area (million)", min_value=1, step=1
+    )
+    number_cases_confirmed = st.sidebar.number_input(
+        "Confirmed cases in your area", min_value=1, step=10
+    )
+    transmission_probability = st.sidebar.slider(
+        label="Probability of a sick person infecting a susceptible person upon contact",
+        min_value=constants.TransmissionRatePerContact.min,
+        max_value=constants.TransmissionRatePerContact.max,
+        value=constants.TransmissionRatePerContact.default,
+    )
+    contact_rate = st.sidebar.slider(
+        label="Number of people infected people come into contact with daily",
+        min_value=constants.AverageDailyContacts.min,
+        max_value=constants.AverageDailyContacts.max,
+        value=constants.AverageDailyContacts.default,
+    )
 
-    num_days_for_prediction = st.sidebar.slider(label='Number of days for prediction', min_value=5, max_value=100, value=50)
+    num_days_for_prediction = st.sidebar.slider(label='Number of days for prediction', min_value=50, max_value=365, value=100)
 
     # Don't know if we want to present doubling time here or R or something else
-    estimated_doubling_time = st.sidebar.slider(label='Estimated time (days) for number of '
-                                                      'infected people to double', min_value=1, max_value=10, value=5)
-
-    symptom_delay =  st.sidebar.slider(label='Time between infection and '
-                                                  'symptoms being displayed', min_value=1, max_value=10, value=5)
-
-    diagnostic_delay = st.sidebar.slider(label='Time between being tested and receiving a diagnosis', min_value=1,
-                                         max_value=10, value=3)
-
+    # estimated_doubling_time = st.sidebar.slider(label='Estimated time (days) for number of infected people to double', min_value=1, max_value=10, value=5)
+    # symptom_delay =  st.sidebar.slider(label='Time between infection and symptoms being displayed', min_value=1, max_value=10, value=5)
+    # diagnostic_delay = st.sidebar.slider(label='Time between being tested and receiving a diagnosis', min_value=1, max_value=10, value=3)
     # https://www.thelancet.com/journals/laninf/article/PIIS1473-3099(20)30195-X/fulltext
-    mortality_rate = st.sidebar.slider(label='% mortality rate', min_value=0., step=.1,
-                                         max_value=50., value=5.)
-
-    hospitalization_rate = st.sidebar.slider(label='% of cases requiring hospitalization',
-                                             min_value=0., max_value=100., step=.5, value=5.)
-
-    ventilator_rate = st.sidebar.slider(label='% of cases requiring a ventilator',
-                                             min_value=0., max_value=100., step=.5, value=1.)
-
-    num_beds = st.sidebar.slider(label='Number of hospital beds available', min_value=100, max_value=1000000, value=100000)
-
-    stay_in_hospital = st.sidebar.slider(label='How many days people stay in hospital', min_value=3, max_value=20, value=12)
+    # mortality_rate = st.sidebar.slider(label='% mortality rate', min_value=0., step=.1, max_value=50., value=5.)
+    # hospitalization_rate = st.sidebar.slider(label='% of cases requiring hospitalization', min_value=0., max_value=100., step=.5, value=5.)
+    # ventilator_rate = st.sidebar.slider(label='% of cases requiring a ventilator',min_value=0., max_value=100., step=.5, value=1.)
+    # num_beds = st.sidebar.slider(label='Number of hospital beds available', min_value=100, max_value=1000000, value=100000)
+    # stay_in_hospital = st.sidebar.slider(label='How many days people stay in hospital', min_value=3, max_value=20, value=12)
 
 
 def run_app():
-    st.title('Corona Calculator')
+    st.title("Corona Calculator")
     sidebar()
-    df = models.case_prediction_df(current_cases=sidebar.number_cases_confirmed,
-                                   doubling_time=sidebar.estimated_doubling_time,
-                                   symptom_delay=sidebar.symptom_delay,
-                                   diagnostic_delay=sidebar.diagnostic_delay,
-                                   max_days=sidebar.num_days_for_prediction)
+    sir_model = models.SIRModel(
+        sidebar.transmission_probability, sidebar.contact_rate, constants.RemovalRate.default
+    )
+    true_cases_estimator = models.TrueInfectedCasesEstimator(constants.AscertainmentRate.default)
+    df = models.get_predictions(
+        true_cases_estimator,
+        sir_model,
+        sidebar.number_cases_confirmed,
+        sidebar.area_population * 1000000,
+        sidebar.num_days_for_prediction
+    )
 
     figure = graphing.infection_graph(df)
     st.write(figure)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_app()
--- models.py
+++ models.py
@@ -1,30 +1,89 @@
-import numpy as np
 import pandas as pd
 
 
-def case_prediction_df(current_cases, doubling_time, symptom_delay, diagnostic_delay, max_days):
-    #TODO @psteeves use the SIR model to make this proper (max_days)
+def get_predictions(cases_estimator, sir_model, num_diagnosed, area_population, max_days):
 
-    # For now, just assume exponential growth
-    time = range(0, max_days)
+    true_cases = cases_estimator.predict(num_diagnosed)
 
-    def calculate_growth(time_step):
-        return current_cases * 2**(time_step / doubling_time)
+    # For now assume removed starts at 0. Doesn't have a huge effect on the model
+    predictions = sir_model.predict(
+        susceptible=area_population - true_cases,
+        infected=true_cases,
+        removed=0,
+        time_steps=max_days,
+    )
+    S, I, R = predictions.values()
 
-    confirmed_cases = [calculate_growth(t) for t in time]
+    num_entries = max_days + 1
+    # Have to use the long format to make plotly express happy
+    df = pd.DataFrame(
+        {
+            "Days": list(range(num_entries)) * 3,
+            "Forecast": S + I + R,
+            "Status": ["Susceptible"] * num_entries
+            + ["Infected"] * num_entries
+            + ["Removed"] * num_entries,
+        }
+    )
+    return df
 
-    # TODO work out true cases properly via ascertainment rate
-    actual_cases = [calculate_growth(t+symptom_delay+diagnostic_delay) for t in time]
 
-    # Have to use this rather clunky format to make plotly express happy
+class TrueInfectedCasesEstimator:
+    """
+    Used to estimate total number of true infected persons based on either number of diagnosed cases or number of deaths.
+    """
 
-    return pd.DataFrame(np.array([2*list(time),
-                                  confirmed_cases + actual_cases,
-                                  ['Confirmed']*max_days + ['Actual']*max_days
-                                  ]).T,
-                        columns=['Days', 'Cases', 'Estimate'])
+    def __init__(self, ascertainment_rate):
+        """
+        :param ascertainment_rate: Ratio of diagnosed to true number of infected persons.
+        """
+        self._ascertainment_rate = ascertainment_rate
+
+    def predict(self, diagnosed_cases):
+        return diagnosed_cases / self._ascertainment_rate
 
 
-if __name__ == '__main__':
-    df = case_prediction_df(1,5,2,2)
-    pd.wide_to_long(df, stubnames=["Confirmed"], i='Days', j="Source")
+class SIRModel:
+    def __init__(self, transmission_rate_per_contact, contact_rate, removal_rate):
+        """
+        :param transmission_rate_per_contact: Prob of contact between infected and susceptible leading to infection.
+        :param contact_rate: Mean number of daily contacts between an infected individual and susceptible people.
+        :param removal_rate: Rate of removal of infected individuals (death or recovery)
+        """
+        self._infection_rate = transmission_rate_per_contact * contact_rate
+        self._removal_rate = removal_rate
+
+    def predict(self, susceptible, infected, removed, time_steps=100):
+        """
+        Run simulation.
+        :param susceptible: Number of susceptible people in population.
+        :param infected: Number of infected people in population.
+        :param removed: Number of recovered people in population.
+        :param time_steps: Time steps to run simulation for
+        :return: List of values for S, I, R over time steps
+        """
+        population = susceptible + infected + removed
+
+        S = [susceptible]
+        I = [infected]
+        R = [removed]
+
+        for t in range(time_steps):
+            s_t = S[-1] - self._infection_rate * I[-1] * S[-1] / population
+            i_t = (
+                I[-1]
+                + self._infection_rate * I[-1] * S[-1] / population
+                - self._removal_rate * I[-1]
+            )
+            r_t = R[-1] + self._removal_rate * I[-1]
+
+            S.append(s_t)
+            I.append(i_t)
+            R.append(r_t)
+
+        return {"S": S, "I": I, "R": R}
+
+
+if __name__ == "__main__":
+    df = get_predictions(1, 5, 2, 2)
+    pd.wide_to_long(df, stubnames=["Confirmed"], i="Days", j="Source")
