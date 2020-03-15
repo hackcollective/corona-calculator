818834ff370b0dd958af3030f39163db1961b60a
Archy de Berler <archy.deberker@gmail.com>
2020-03-15 19:00:52 -0400

    borders light grey

--- interface/elements.py
+++ interface/elements.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def reported_vs_true_cases(num_cases_confirmed, num_cases_estimated):
-    _border_color = 'gray'
+    _border_color = 'light-gray'
     _number_format = 'font-size:35px; font-style:bold;'
     _cell_style = f' border: 2px solid {_border_color}; border-bottom:2px solid white; margin:10px'
     st.markdown(f"<table style='width: 100%; font-size:14px;  border: 0px solid gray; border-spacing: 10px;  border-collapse: collapse;'> "
