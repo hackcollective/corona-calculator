ea179ba35dd8285d9716742628bcd2a384d08347
Pat Steeves <patrick.steeves@elementai.com>
2020-03-14 22:16:50 -0400

    remove unused sliders and make latest fetch time accurate

--- fetch_live_data.py
+++ fetch_live_data.py
@@ -1,3 +1,4 @@
+import datetime
 import shutil
 import subprocess
 from pathlib import Path
@@ -10,6 +11,8 @@ REPO_DIRPATH = "COVID-19"
 DAILY_REPORTS_DIRPATH = "COVID-19/csse_covid_19_data/csse_covid_19_daily_reports"
 
 OUTPUT_PATH = "data/latest_disease_data.csv"
+LOG_PATH = "data/latest_fetch.log"
+DATESTRING_FORMAT = "%Y%m%d%H%M"
 
 
 def execute_shell_command(command: List[str]):
@@ -34,42 +37,45 @@ def get_latest_filepath(csv_filepaths: List[str]) -> Tuple[str, int, int, int]:
     return file_infos[-1]
 
 
-# Clone the JHU COVID GitHub repo (takes about a minute)
-cmd = ["git", "clone", GITHUB_REPO]
-execute_shell_command(cmd)
+if __name__ == "__main__":
+    # Clone the JHU COVID GitHub repo (takes about a minute)
+    cmd = ["git", "clone", GITHUB_REPO]
+    execute_shell_command(cmd)
 
-# Go to daily reports directory and fetch all CSV files
-csv_filepaths = Path(DAILY_REPORTS_DIRPATH).glob("*.csv")
-# Get the filepath for latest report
-latest_filepath, year, month, day = get_latest_filepath(csv_filepaths)
-print(year, month, day, latest_filepath)
+    # Go to daily reports directory and fetch all CSV files
+    csv_filepaths = Path(DAILY_REPORTS_DIRPATH).glob("*.csv")
+    # Get the filepath for latest report
+    latest_filepath, year, month, day = get_latest_filepath(csv_filepaths)
+    print(year, month, day, latest_filepath)
 
-# Read the report into a pandas df
-df = pd.read_csv(latest_filepath)
-# Aggregate the country stats
-country_stats_df = df.groupby("Country/Region").agg(
-    {"Confirmed": "sum", "Deaths": "sum", "Recovered": "sum"}
-)
+    # Read the report into a pandas df
+    df = pd.read_csv(latest_filepath)
+    # Aggregate the country stats
+    country_stats_df = df.groupby("Country/Region").agg(
+        {"Confirmed": "sum", "Deaths": "sum", "Recovered": "sum"}
+    )
 
-# Drop countries for which we have no demographic data
-country_stats_df.drop(
-    [
-        "Brunei",
-        "Cruise Ship",
-        "French Guiana",
-        "Guadeloupe",
-        "Holy See",
-        "Martinique",
-        "North Macedonia",
-        "Reunion",
-    ],
-    inplace=True,
-)
+    # Drop countries for which we have no demographic data
+    country_stats_df.drop(
+        [
+            "Brunei",
+            "Cruise Ship",
+            "French Guiana",
+            "Guadeloupe",
+            "Holy See",
+            "Martinique",
+            "North Macedonia",
+            "Reunion",
+        ],
+        inplace=True,
+    )
 
-# Write out the country stats to output path
-# The CSV file contains 4 columns:
-## 1) Country/Region, 2) Confirmed, 3)	Deaths, 4) Recovered
-country_stats_df.to_csv(OUTPUT_PATH, index=True)
+    # Write out the country stats to output path
+    # The CSV file contains 4 columns:
+    ## 1) Country/Region, 2) Confirmed, 3)	Deaths, 4) Recovered
+    country_stats_df.to_csv(OUTPUT_PATH, index=True)
+    with open(LOG_PATH, "w") as f:
+        f.write(datetime.datetime.now().strftime(DATESTRING_FORMAT))
 
-# # Remove GitHub repo directory
-shutil.rmtree(REPO_DIRPATH)
+    # # Remove GitHub repo directory
+    shutil.rmtree(REPO_DIRPATH)
