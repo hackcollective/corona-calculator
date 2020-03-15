2d2129e34329bea2ae02ea49e2f8d2349fa1e376
Archy de Berler <archy.deberker@gmail.com>
2020-03-15 11:57:42 -0400

    Change graphs to white

--- graphing.py
+++ graphing.py
@@ -10,21 +10,21 @@ def plot_true_versus_confirmed(confirmed, predicted):
             "Color": ["b", "r"],
         }
     )
-    fig = px.bar(df, x="Status", y="Cases", color="Color")
+    fig = px.bar(df, x="Status", y="Cases", color="Color", template='plotly_white')
     fig.layout.update(showlegend=False)
 
     return fig
 
 
 def infection_graph(df):
-    fig = px.line(df, x="Days", y="Forecast", color="Status")
+    fig = px.line(df, x="Days", y="Forecast", color="Status", template='plotly_white')
     # fig.add_scatter(df, x='Days', y='Patients in Hospital')
     # fig.add_scatter(df, x='Days', y='Deaths')
 
     return fig
 
 
-def hospitalization_graph(df, number_of_beds, number_of_ventilators):
+def hospitalization_graph(df, number_of_beds, number_of_ventilators=None):
     # Add in the number of beds and number of ventilators to the df
     days = list(df.Days.unique())
     n_days = len(days)
@@ -37,15 +37,7 @@ def hospitalization_graph(df, number_of_beds, number_of_ventilators):
         }
     )
 
-    # ventilator_df = pd.DataFrame(
-    #     {
-    #         "Days": days,
-    #         "Forecast": [number_of_ventilators] * n_days,
-    #         "Status": ["Number of Ventilators"] * n_days,
-    #     }
-    # )
-
-    fig = px.line(df, x="Days", y="Forecast", color="Status")
+    fig = px.line(df, x="Days", y="Forecast", color="Status", template='plotly_white')
     fig.add_scatter(
         x=bed_df.Days,
         y=bed_df.Forecast,
@@ -55,13 +47,6 @@ def hospitalization_graph(df, number_of_beds, number_of_ventilators):
         fillcolor="rgba(255,0,0,.1)",
         line={"color": "rgba(255,0,0,.5)"},
     )
-    # fig.add_scatter(
-    #     x=ventilator_df.Days,
-    #     y=ventilator_df.Forecast,
-    #     name="Number of Ventilators",
-    #     fill="tozeroy",
-    #     opacity=0.1,
-    #     fillcolor="rgba(0,255,0,.1)",
-    # )
+
 
     return fig
