78e4ec9bcf23335b1352d2778b23ac89a34368ba
Archy de Berler <archy.deberker@gmail.com>
2020-03-18 22:30:31 -0400

    Add a Github logo with link to repo

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 COLOR_MAP = {"default": "#262730", "pink": "#E22A5B", "purple": "#985FFF"}
 
 
@@ -24,3 +26,12 @@ def generate_html(
 
 
 graph_warning = "Please be aware the scale of this graph changes!"
+
+
+def insert_github_logo():
+    st.markdown('<br>'
+                '<div style="text-align: center;">'
+                '<a href="https://github.com/archydeberker/corona-calculator"> '
+                '<img src="https://image.flaticon.com/icons/png/128/1051/1051326.png" width=64>'
+                ' </img>'
+                '</a> </div>', unsafe_allow_html=True)
